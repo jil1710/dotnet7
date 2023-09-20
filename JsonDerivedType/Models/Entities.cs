abc7c4df9a7286d446c27a0e702d9233360143f4
jil <jil.p@simformsolutions.com>
2023-09-20 10:04:29 +0530

    add: jsonderive type attribute

---
+++ JsonDerivedType/Models/Entities.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace JsonDerivedType.Models
+{
+    [JsonDerivedType(typeof(Student),"student")]
+    [JsonDerivedType(typeof(Professor),"professor")]
+    public class Member
+    {
+        public string? Name { get; set; }
+
+        public DateTime BirthDate { get; set; }
+    }
+    public class Student : Member
+    {
+        public int RegistrationYear { get; set; }
+
+        public List<string> Courses { get; set; } = new List<string>();
+    }
+    public class Professor : Member
+    {
+        public string? Rank { get; set; }
+
+        public bool IsTenured { get; set; }
+    }
+}
---
+++ JsonDerivedType/Program.cs
@@ -0,0 +1,21 @@
+﻿using JsonDerivedType.Models;
+using System.Text.Json;
+
+namespace JsonDerivedType
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Member member = new Member() { BirthDate = DateTime.Now, Name = "Jhon"};
+            var memberSerializer = JsonSerializer.Serialize(member);
+            Member memberDeserialize = JsonSerializer.Deserialize<Member>(memberSerializer)!;
+
+            Member student = new Student() { Name="Jhon",BirthDate=DateTime.Now,Courses = new List<string> {"PHP"},RegistrationYear=2023};    
+            var studentSerializer = JsonSerializer.Serialize(student);
+            Student studentDeserialize = JsonSerializer.Deserialize<Student>(studentSerializer)!;
+
+            Console.ReadKey();
+        }
+    }
+}
